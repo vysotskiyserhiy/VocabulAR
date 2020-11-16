eae41024a690b8052d4d2898ce9f15604667bb0c
Serge Vysotsky <vysotskiyserhiy@gmail.com>
2020-11-16 13:33:48 +0200

    Initial Commit

---
+++ VocabulAR/VocabulARApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabulARApp.swift
+//  VocabulAR
+//
+//  Created by Serge Vysotsky on 16.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct VocabulARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
